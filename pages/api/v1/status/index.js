9489063283ad3ea7839031c2e72f9f15d9b9ae0c
Israel Moreira <israelmessias96@gmail.com>
2026-01-08 22:21:28 -0300

    alterada mensagem de resposta da API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(request, response) {
-  response.status(200).json({ chave: "São acima da média" });
+  response.status(200).json({ chave: "Meu novo site está a caminho, além de usar para me divulgar farei um blog pessoal" });
 }
 
 export default status;
