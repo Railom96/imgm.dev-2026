24a0caa8309c85805cb4869a379c944cf06f6a65
Israel Moreira <israelmessias96@gmail.com>
2026-06-23 03:55:36 -0300

    chore: adiciona github action para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://imgm.dev.br',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
