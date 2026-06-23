ff373598b3fda70feb74d1a73d349d4c227be838
Israel Moreira <israelmessias96@gmail.com>
2026-06-23 03:34:27 -0300

    modified:   src/components/Footer.astro 	modified:   src/components/Header.astro 	modified:   src/consts.ts 	deleted:    src/content/blog/first-post.md 	deleted:    src/content/blog/markdown-style-guide.md 	deleted:    src/content/blog/second-post.md 	deleted:    src/content/blog/third-post.md 	deleted:    src/content/blog/using-mdx.mdx 	modified:   src/pages/index.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Israel Messias';
+export const SITE_DESCRIPTION = 'Desenvolvedor e Especialista em IAM';
